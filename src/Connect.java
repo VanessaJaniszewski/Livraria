111e97a6730314e4b498f3a15e1475b3eb71d0ec
VanssaJaniszewski <vjaniszewski@gmail.com>
2022-11-11 10:46:34 -0300

    changed connection to a class with only connection methods

--- src/Connect.java
+++ src/Connect.java
@@ -1,40 +1,40 @@
-//
-//
-//import javax.persistence.EntityManager;
-//import javax.persistence.EntityManagerFactory;
-//import javax.persistence.Persistence;
-//
-//public class Connect {
-//	
-//	
-//	public void find(Integer id) {
-//		EntityManagerFactory emf = Persistence.createEntityManagerFactory("Oracle");;
-//		EntityManager em = emf.createEntityManager();
-//		try {
-//			
-//			Livro l1 = em.find(Livro.class, id);
-//			em.getTransaction().begin();
-//			em.merge(l1);
-//			em.getTransaction().commit();
-//			
-//		}catch (Exception e) {
-//		System.err.println(e);
-//		}finally {
-//		em.close();
-//	}}
-//	
-//	public void add(Livro livro) {
-//		EntityManagerFactory emf = Persistence.createEntityManagerFactory("Oracle");;
-//		EntityManager em = emf.createEntityManager();
-//		try {
-//			em.getTransaction().begin();
-//			em.persist(livro);
-//			em.getTransaction().commit();
-//			
-//		}catch (Exception e) {
-//		System.err.println(e);
-//		}finally {
-//		em.close();
-//	}}
-//
-//}
+
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Connect {
+	
+	
+	public void find(Integer id) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("Oracle");;
+		EntityManager em = emf.createEntityManager();
+		try {
+			
+			Livro l1 = em.find(Livro.class, id);
+			em.getTransaction().begin();
+			em.merge(l1);
+			em.getTransaction().commit();
+			
+		}catch (Exception e) {
+		System.err.println(e);
+		}finally {
+		em.close();
+	}}
+	
+	public void add(Livro livro) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("Oracle");;
+		EntityManager em = emf.createEntityManager();
+		try {
+			em.getTransaction().begin();
+			em.persist(livro);
+			em.getTransaction().commit();
+			
+		}catch (Exception e) {
+		System.err.println(e);
+		}finally {
+		em.close();
+	}}
+
+}
--- src/Teste.java
+++ src/Teste.java
@@ -1,25 +1,9 @@
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
 public class Teste {
 	public static void main(String[] args) {
 		
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("Oracle");
-		EntityManager em = emf.createEntityManager();
+		Connect c = new Connect();
 		Livro l1 = new Livro("Flowers for Algernon", "Daniel Keyes", null);
-		
-		try {
-			em.getTransaction().begin();
-			em.persist(l1);
-			em.getTransaction().commit();
-		}catch (Exception e) {
-			System.err.println(e);
-			em.getTransaction().rollback();
-		}finally {
-			em.close();
-		}
-		
+		c.add(l1);
 		
 	}
 
